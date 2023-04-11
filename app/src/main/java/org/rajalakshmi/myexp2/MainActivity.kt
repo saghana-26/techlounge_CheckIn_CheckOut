be5e6314ab1245756db497fcac8c21f9c3ec881e
saghana <saghanaraj06@gmail.com>
2023-04-11 14:14:27 +0530

    Initial commit

---
+++ app/src/main/java/org/rajalakshmi/myexp2/MainActivity.kt
@@ -0,0 +1,15 @@
+package org.rajalakshmi.myexp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
